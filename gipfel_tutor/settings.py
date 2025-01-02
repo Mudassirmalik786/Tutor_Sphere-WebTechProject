e7e55c3581d85666ac132846d0df74d64a803891
AsadUllah-11 <120157110+AsadUllah-11@users.noreply.github.com>
2025-01-02 16:07:38 +0500

    Update settings.py

--- gipfel_tutor/settings.py
+++ gipfel_tutor/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 # Separate Development variable for db, email backend and S3 bucket
 # DEVELOPMENT = os.environ.get("DEVELOPMENT", "") == "False"
-DEBUG = False
+DEBUG = True
 DEVELOPMENT = True
 ALLOWED_HOSTS = [
     ".herokuapp.com",
